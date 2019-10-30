8281997725bc0b98b12a637ccaa25e7b7d633a81
Rahul Shukla <rrshukla107@gmail.com>
2019-10-29 17:16:36 -0700

    Initial Commit

---
+++ src/test/java/com/rahul/AppTest.java
@@ -0,0 +1,18 @@
+package com.rahul;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        Assertions.assertTrue(true);
+    }
+}
